ed3836609cc637bbc273e00fcd2ccb3cdaa31a73
Karan Kumar <kumarkay.ca@gmail.com>
2020-01-09 16:56:11 -0500

    user location on map

--- iOS_Location/ViewController.swift
+++ iOS_Location/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     //create a variable of the CL_LOCATION_MANAGER
     var locationManager = CLLocationManager()
     
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -21,16 +23,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        mapView.showsUserLocation=true
         
         
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
+        
         // getting the user location
         let userlocation :CLLocation = locations[0]
     // print(userlocation)
         
+        let lat = userlocation.coordinate.latitude
+        let long = userlocation.coordinate.longitude
+        
+        let latDelta : CLLocationDegrees = 0.05
+        let longDelta : CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location = CLLocationCoordinate2D (latitude: lat, longitude: long)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: true)
       //finding the user address from the user location
         CLGeocoder().reverseGeocodeLocation(userlocation){
             (placemark,error) in
